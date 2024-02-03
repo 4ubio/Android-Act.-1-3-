a62385c035a68dc5c3adf5d583fc48d35092f75a
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-02-03 13:01:05 -0600

    Act 2 (New V)

--- app/src/main/java/com/example/columnsandrows/components/Actividad2.kt
+++ app/src/main/java/com/example/columnsandrows/components/Actividad2.kt
@@ -1,10 +1,9 @@
 package com.example.columnsandrows.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,224 +13,67 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun Block(text: String, color: Color, background: Color) {
+    Box(
+        modifier = Modifier
+            .padding(3.dp)
+            .background(background)
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text(
+            text = text,
+            color = color,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun Act2(){
+fun Act22(){
     Row(
-        modifier = Modifier
-            .height(200.dp)
-            .width(200.dp)
-            .background(Color(0xFFBBADA0)),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier.width(200.dp).background(Color(0xFFBBADA0))
     ) {
         Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(3.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.weight(1f)
         ) {
-            Text(
-                text = "16",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF59563))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = " 128",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFEDCF72))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 1.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "32",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF67C5F))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "16",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF59563))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
+            Block(text = "16", color = Color(0xFFFFFFFF), background = Color(0xFFF59563))
+            Block(text = "128", color = Color(0xFFFFFFFF), background = Color(0xFFEDCF72))
+            Block(text = "32", color = Color(0xFFFFFFFF), background = Color(0xFFF67C5F))
+            Block(text = "16", color = Color(0xFFFFFFFF), background = Color(0xFFF59563))
         }
-
         Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(3.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
         ) {
-            Text(
-                text = "",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFCDC1B4))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "16",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF59563))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "64",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF65E3B))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "8",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF2B179))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 17.dp)
-            )
+            Block(text = "", color = Color(0xFFFFFFFF), background = Color(0xFFCDC1B4))
+            Block(text = "16", color = Color(0xFFFFFFFF), background = Color(0xFFF59563))
+            Block(text = "64", color = Color(0xFFFFFFFF), background = Color(0xFFF65E3B))
+            Block(text = "8", color = Color(0xFFFFFFFF), background = Color(0xFFF2B179))
         }
-
         Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(3.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
         ) {
-            Text(
-                text = "",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFCDC1B4))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFCDC1B4))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "2",
-                color = Color(0xFF000000),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFEEE4DA))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 17.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "8",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFF2B179))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 17.dp)
-            )
+            Block(text = "", color = Color(0xFFFFFFFF), background = Color(0xFFCDC1B4))
+            Block(text = "", color = Color(0xFFFFFFFF), background = Color(0xFFCDC1B4))
+            Block(text = "2", color = Color(0xFF000000), background = Color(0xFFEEE4DA))
+            Block(text = "8", color = Color(0xFFFFFFFF), background = Color(0xFFF2B179))
         }
-
         Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(3.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)
         ) {
-            Text(
-                text = "4",
-                color = Color(0xFF070707),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFEDE0C8))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 17.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFCDC1B4))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "",
-                color = Color(0xFFFFFFFF),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFCDC1B4))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 10.dp)
-            )
-            Spacer(modifier = Modifier.height(height = 5.dp))
-            Text(
-                text = "2",
-                color = Color(0xFF000000),
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .background(Color(0xFFEEE4DA))
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, start = 17.dp)
-            )
+            Block(text = "4", color = Color(0xFF000000), background = Color(0xFFEDE0C8))
+            Block(text = "", color = Color(0xFFFFFFFF), background = Color(0xFFCDC1B4))
+            Block(text = "", color = Color(0xFFFFFFFF), background = Color(0xFFCDC1B4))
+            Block(text = "2", color = Color(0xFF000000), background = Color(0xFFEEE4DA))
         }
     }
-
 }
